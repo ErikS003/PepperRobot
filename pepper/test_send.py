95a57d504b8f43931e28c03f7c60110d2aa9e598
Erik Schahine <erik.schahine@ericsson.com>
2025-06-23 13:36:32 +0200

    simple script to send an existing audio file from pepper to server

---
+++ pepper/test_send.py
@@ -0,0 +1,25 @@
+
+test_send.py
+import time
+import requests
+
+start_time = time.time()
+url = "http://1516-192-176-1-79.ngrok-free.app/transcribe"
+
+files = {
+    "audio": (
+        "test_record.ogg",
+        open("/home/nao/recordings/test_record.ogg", "rb"),
+        "audio/ogg"
+    )
+}
+
+try:
+    resp = requests.post(url, files=files, timeout=100)
+    print(resp.status_code, resp.json())
+except Exception as e:
+    print("Request failed:", e)
+
+
+end_time = time.time()
+print("elapsed time:", end_time-start_time)
