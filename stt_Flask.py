f11b77639717b01eb048867a4c45595232cc3398
Erik Schahine <erik.schahine@ericsson.com>
2025-06-23 11:33:40 +0200

    Changed to FastAPI from flask + pre loading bytes for latency

--- stt_Flask.py
+++ stt_Flask.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, UploadFile, File, Request, HTTPException
+from fastapi.responses import JSONResponse
 from faster_whisper import WhisperModel
 import os
 import tempfile
 from huggingface_hub import InferenceApi,InferenceClient
 import time
+import io
+import uvicorn
 
-
-app = Flask("speech_to_text")
-model = WhisperModel("base", device="cpu", compute_type="int8")
+app = FastAPI(title="speech_to_text")
+model = WhisperModel("small", device="cpu", compute_type="int8")
 
 #inference sample
 HF_TOKEN = "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"
@@ -17,51 +19,44 @@ client = InferenceClient(
 )
 
 
-@app.route("/transcribe", methods=["POST"])
+@app.post("/transcribe")
 
-def transcribe():
+async def transcribe(request: Request, audio: UploadFile):
+    print(f"Transcribing file")
+
+    if not audio.content_type.startswith("audio/"):
+        raise HTTPException(400, "Not an audio file")
     t11 = time.time()
-    print(f"Transcribing file from {request.remote_addr}")
-    f = request.files.get("audio")
-    if not f or not f.mimetype.startswith("audio/"):
-        return jsonify({"error": "no valid audio file"}), 400
+    audio_bytes = await audio.read()
+    buf = io.BytesIO(audio_bytes)
     t12 = time.time()
-    # time to recieve file
-    # create a unique temp file
-    with tempfile.NamedTemporaryFile(suffix=".ogg", delete=False) as tmp:
-        tmp_path = tmp.name
-        f.save(tmp_path)
 
-    try:
-        t21=time.time()
-        segments, info = model.transcribe(tmp_path)
-        text = "".join(seg.text for seg in segments)
-        prompt = (
-                f"*PRE PROMPT HERE*\n Prompt:\n{text}\n\n:")
-        t22=time.time() # time for transcription
-        print("time for STT transcription: ", t22-t21)
-        t31=time.time()
-        completion = client.chat.completions.create(
-        model="mistralai/Mistral-7B-Instruct-v0.3",
-        messages=[
-            {
-                "role": "user",
-                "content": f"{prompt}"
-            }
-        ],
-        )
-        answer= completion.choices[0].message
-        t32 = time.time() #time for llm answer
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-    finally:
-        try:
-            os.remove(tmp_path)
-        except OSError:
-            pass
+
+    t21=time.time()
+    segments, info = model.transcribe(buf)
+    text = "".join(seg.text for seg in segments)
+    prompt = (
+            f"*PRE PROMPT HERE*\n Prompt:\n{text}\n\n:")
+    t22=time.time() # time for transcription
+    print("time for STT transcription: ", t22-t21)
+    t31=time.time()
+    completion = client.chat.completions.create(
+    model="mistralai/Mistral-7B-Instruct-v0.3",
+    messages=[
+        {
+            "role": "user",
+            "content": f"{prompt}"
+        }
+    ],
+    )
+    answer= completion.choices[0].message
+    t32 = time.time() #time for llm answer
     print(answer)
-    return jsonify({"answer": answer,"times": f'{t12-t11},{t22-t21},{t32-t31}'})
-
+    return JSONResponse({
+        "answer": answer,
+        "times": [round(t12-t11,3), round(t22-t21,3), round(t32-t31,3)]
+    })
 if __name__ == "__main__":
     # listen on all interfaces
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
